176976f70da4035172d5b10e921f5ad3f74e3637
Jhonatan Giraldo <jhonatan.giraldo@autodesk.com>
2020-04-12 00:03:09 -0500

    clean code: remove extra dependencies, change some fields, and add some logic to app.js 2020/april/11

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  return <div>Hey Guys!</div>;
-}
+  const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+  }, []);
+
+  const increment = () => {
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+  };
+
+  const decrement = () => {
+    const newCounter = counter - 1;
+    setCounter(newCounter);
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      {counter}
+      <button onClick={decrement}>Decrement</button>
+      <label>Hey Guys!</label>
+    </div>
+  );
+}
