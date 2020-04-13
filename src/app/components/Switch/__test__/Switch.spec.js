fac44cea60d3d5f6a9c656418ad6bb9390e3db19
Jhonatan Giraldo <jhonatan.giraldo@autodesk.com>
2020-04-12 21:11:05 -0500

    improved jest: Jest watch, coverage, and jest debugger 2020/april/12

--- src/app/components/Switch/__test__/Switch.spec.js
+++ src/app/components/Switch/__test__/Switch.spec.js
@@ -5,6 +5,7 @@ import Switch from '../';
 describe('Switch component test', () => {
   it('should have an initial 0 status', () => {
     const render = shallow(<Switch />);
+    debugger;
     expect(render.text()).toEqual('Status: 0');
   });
 });
--- test/jestsetup.js
+++ test/jestsetup.js
@@ -2,3 +2,10 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+/**
+ * To debug jest applications:
+ * 1. chrome://inspect
+ * 2. Open dedicated DevTools for Node
+ * 3. Run either npm run test or yarn test
+ */
